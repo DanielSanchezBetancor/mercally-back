bd67dcd28bf5aa3dc6e148abe25a3844ef15861c
Daniel Sanchez Betancor <danielsanchezpro@gmail.com>
2024-09-09 20:14:17 +0200

    @1.mercally @Refactor imports and add data tables to dummies.ts

--- scripts/init-seed.ts
+++ scripts/init-seed.ts
@@ -1,27 +1,27 @@
 import Logger from "@coding-flavour/logger"
 import { getConnection } from "../orm/base/connection"
-import Price, { PricesFields } from "../orm/prices/base"
-import Products from "../orm/products/Products"
-import { PRICES, HISTORY_PRICES, PRODUCTS, SHOPPING_LIST, SHOPPING_LIST_PRODUCTS, USER_SHOPPING_LIST, USERS_STORES, STORES, USER_PREFERENCES, USER, CATEGORIES } from "./dummies"
-import { ProductsFields } from "../orm/products/base"
-import { HistoryPricesFields } from "../orm/HistoryPrices/HistoryPricesBase"
-import HistoryPrice from "../orm/HistoryPrices/HistoryPrices"
-import { ShoppingList } from "../orm/ShoppingLists/ShoppingListsBase"
-import ShoppingLists from "../orm/ShoppingLists/ShoppingLists"
-import { ProductsShoppingLists } from "../orm/ProductsShoppingLists/ProductsShoppingLists"
-import { ProductsShoppingList } from "../orm/ProductsShoppingLists/ProductsShoppingListsBase"
-import { UserShoppingList, UserShoppingListRequest } from "../orm/UserShoppingLists/UserShoppingListsBase"
-import { UserShoppingLists } from "../orm/UserShoppingLists/UserShoppingLists"
-import { UserStore } from "../orm/UsersStores/UsersStoresBase"
-import { UsersStores } from "../orm/UsersStores/UsersStores"
-import { Store } from "../orm/stores/base/StoresBase"
-import { Stores } from "../orm/stores/Stores"
-import { User } from "../orm/users/UsersBase"
-import { Users } from "../orm/users/Users"
-import { UserSetting } from "../orm/UsersSettings/UsersSettingsBase"
-import { UsersSettings } from "../orm/UsersSettings/UsersSettings"
 import { CategoryTableData } from "../orm/categories/base/CategoriesBase"
 import Categories from "../orm/categories/Categories"
+import HistoryPrice from "../orm/HistoryPrices/HistoryPrices"
+import { HistoryPricesFields } from "../orm/HistoryPrices/HistoryPricesBase"
+import Price, { PricesFields } from "../orm/prices/base"
+import { ProductsFields } from "../orm/products/base"
+import Products from "../orm/products/Products"
+import { ProductsShoppingLists } from "../orm/ProductsShoppingLists/ProductsShoppingLists"
+import { ProductsShoppingList } from "../orm/ProductsShoppingLists/ProductsShoppingListsBase"
+import ShoppingLists from "../orm/ShoppingLists/ShoppingLists"
+import { ShoppingList } from "../orm/ShoppingLists/ShoppingListsBase"
+import { Store } from "../orm/stores/base/StoresBase"
+import { Stores } from "../orm/stores/Stores"
+import { Users } from "../orm/users/Users"
+import { User } from "../orm/users/UsersBase"
+import { UserShoppingLists } from "../orm/UserShoppingLists/UserShoppingLists"
+import { UserShoppingList, UserShoppingListRequest } from "../orm/UserShoppingLists/UserShoppingListsBase"
+import { UsersSettings } from "../orm/UsersSettings/UsersSettings"
+import { UserSetting } from "../orm/UsersSettings/UsersSettingsBase"
+import { UsersStores } from "../orm/UsersStores/UsersStores"
+import { UserStore } from "../orm/UsersStores/UsersStoresBase"
+import { CATEGORIES, HISTORY_PRICES, PRICES, PRODUCTS, SHOPPING_LIST_PRODUCTS, SHOPPING_LISTS, STORES, USER, USER_PREFERENCES, USER_SHOPPING_LISTS, USERS_STORES } from "./dummies"
 
 // Lo suyo seria importar todos los 'base/orm' y recoger todos los 'tableNames' de cada uno, asi no acoplamos el script a las tablas
 const tables = [
@@ -119,6 +119,8 @@ async function createTables(conn: ReturnType<typeof getConnection>) {
     CREATE TABLE IF NOT EXISTS shopping_lists (
       id INTEGER AUTO_INCREMENT,
       code VARCHAR(255),
+      name VARCHAR(255) NOT NULL,
+      id_background INTEGER NOT NULL,
       PRIMARY KEY (id)
     )
   `)
@@ -157,13 +159,12 @@ async function createTables(conn: ReturnType<typeof getConnection>) {
 }
 
 async function insertDummyData(
-  conn: ReturnType<typeof getConnection>,
   products: ProductsFields[],
   prices: PricesFields[],
   historyPrices: HistoryPricesFields[],
-  shoppingList: ShoppingList,
+  shoppingList: ShoppingList[],
   productsShoppingLists: ProductsShoppingList[],
-  userShoppingLists: UserShoppingList,
+  userShoppingLists: UserShoppingList[],
   usersStores: UserStore[],
   stores: Store[],
   userPreferences: UserSetting,
@@ -187,8 +188,9 @@ async function insertDummyData(
     await new Products().insert(product)
   }
 
-  await new ShoppingLists().insert(shoppingList)
-
+  for (const shoppingListRow of shoppingList) {
+    await new ShoppingLists().insert(shoppingListRow)
+  }
 
   for (const productShoppingList of productsShoppingLists) {
     await new ProductsShoppingLists().insert(productShoppingList)
@@ -206,7 +208,9 @@ async function insertDummyData(
     await new UsersStores().insert(userStore)
   }
 
-  await new UserShoppingLists().insert(userShoppingLists)
+  for (const userShoppingList of userShoppingLists) {
+    await new UserShoppingLists().insert(userShoppingList)
+  }
 
 
   logger.log('Dummy data inserted', { subtractLevel: true })
@@ -230,9 +234,9 @@ async function init(
   products = PRODUCTS,
   prices = PRICES,
   historyPrices = HISTORY_PRICES,
-  shoppingList = SHOPPING_LIST,
+  shoppingList = SHOPPING_LISTS,
   productsShoppingLists = SHOPPING_LIST_PRODUCTS,
-  userShoppingLists = USER_SHOPPING_LIST,
+  userShoppingLists = USER_SHOPPING_LISTS,
   usersStores = USERS_STORES,
   stores = STORES,
   userPreferences = USER_PREFERENCES,
@@ -243,7 +247,7 @@ async function init(
   const conn = getConnection()
   await dropTables(conn)
   await createTables(conn)
-  await insertDummyData(conn,
+  await insertDummyData(
     products,
     prices,
     historyPrices,
